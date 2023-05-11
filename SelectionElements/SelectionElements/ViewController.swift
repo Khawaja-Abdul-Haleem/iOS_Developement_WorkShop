8e488f7e5d69405e787bcf494ed8a82afa130ce6
khawajaabdulhaleem <haleemabdul194@gmail.com>
2023-05-11 17:30:27 +0500

    SelectionElement modified

--- SelectionElements/SelectionElements/ViewController.swift
+++ SelectionElements/SelectionElements/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -85,11 +86,36 @@ class ViewController: UIViewController {
     }
     
     
+    
+    @IBOutlet weak var textOneLine: UILabel!
+    
+    var isChecked = false
+    var imageAttachment = NSTextAttachment()
+    let imageOffsetY: CGFloat = -5.0
+    var attachmentString: NSAttributedString = NSAttributedString()
+    var completeText: NSMutableAttributedString = NSMutableAttributedString()
+    var textAfterIcon: NSAttributedString = NSAttributedString()
+    
+    let descriptionText = "Check The Box to indicate that you are atleast 18 years of age, agree to Term and condition and privacyPolicy."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        
+        initialSetup()
         
+    }
+    
+    @objc func userTappedOnLink() {
+        if isChecked == false {
+            setImageToLabel(imageName: "checked", emptySpace: " ")
+            
+            isChecked = true
+        } else {
+          
+            setImageToLabel(imageName: "unchecked", emptySpace: "")
+            isChecked = false
+        }
     }
     
     @objc func maleValueChange(_ sender: CheckBox) {
@@ -108,6 +134,33 @@ class ViewController: UIViewController {
          
        }
    
+    func initialSetup() {
+        
+         setImageToLabel(imageName: "unchecked", emptySpace: "")
+       
+         let gesture = UITapGestureRecognizer(target: self, action: #selector(userTappedOnLink))
+         // if labelView is not set userInteractionEnabled, you must do so
+         textOneLine.isUserInteractionEnabled = true
+         textOneLine.addGestureRecognizer(gesture)
+    }
+    
+    func setImageToLabel(imageName: String, emptySpace: String) {
+        imageAttachment.image = UIImage(named: imageName)
+        // Set bound to reposition
+
+        imageAttachment.bounds = CGRect(x: 0, y: imageOffsetY, width: 30, height: 30)
+        // Create string with attachment
+         attachmentString = NSAttributedString(attachment: imageAttachment)
+        // Initialize mutable string
+        completeText = NSMutableAttributedString(string: emptySpace)
+        // Add image to mutable string
+        completeText.append(attachmentString)
+        // Add your text to mutable string
+        textAfterIcon = NSAttributedString(string: descriptionText)
+        completeText.append(textAfterIcon)
+        self.textOneLine.textAlignment = .left
+        self.textOneLine.attributedText = completeText
+    }
    
 
 }
